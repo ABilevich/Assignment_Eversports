f3c2263f40d60a20ce2d9d0fa459efeea9f5236b
Andres Bilevich <andresbilevich@gmail.com>
2025-11-22 10:56:16 +0100

    added all types to app.types

--- applications/next-boilerplate-client/src/app/app.types.ts
+++ applications/next-boilerplate-client/src/app/app.types.ts
@@ -1,4 +1,18 @@
 export interface Product {
   id: string
   name: string
+  imageUrl?: string
+}
+
+export interface User {
+  id: string
+  firstName: string
+  lastName: string
+}
+
+export interface Purchase {
+  id: string
+  date: string
+  user: User
+  product: Product
 }
